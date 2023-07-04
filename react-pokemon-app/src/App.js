5d176c8effcca90cfaeebf1ad590aa48a8514df4
Valeria Serci <balerty84@gmail.com>
2023-07-04 22:57:00 +0100

    add a function at the input and a placeholder

--- react-pokemon-app/src/App.js
+++ react-pokemon-app/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [pokemonName, setPokemonName] = useState('');
+
+  const onInput = (event) => {
+    setPokemonName(event.target.value);
+  } 
+
   return (
     <div className="App">
       <div className="TitleSection">
         <h1>Pokemon</h1>
-        <input type="text" />
+        <input type="text" onChange={onInput} placeholder='Name...'/>
         <button>Search Pokemon</button>
       </div>
     </div>
